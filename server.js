5fcfc31014006df8cbf9ead9cd84bb0cab1d9304
Sameer Khurd <sameerkhurd@gmail.com>
2021-03-06 17:45:56 +0530

    Deploy conf 0.1

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/CovidIndiaLive'));
+app.use(express.static('./www'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/CovidIndiaLive/'}),
+    res.sendFile('index.html', {root: 'www/'}),
 );
 
 // Start the app by listening on the default Heroku port
